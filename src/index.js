9cae3881fddae68a1ce1cca2b07bc2a519762863
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-06-11 13:13:50 +0530

    upgrade to react 18

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-import {BaseProvider, LightTheme} from 'baseui';
-import { Provider as StyletronProvider } from "styletron-react";
-import { Client as Styletron } from "styletron-engine-atomic";
-const engine = new Styletron();
-
-const rootElement = document.getElementById("root");
-
-ReactDOM.render(
-  <StyletronProvider value={engine}>
-    <BaseProvider theme={LightTheme}>
-      <App />
-    </BaseProvider>
-  </StyletronProvider>,
-  rootElement
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />, container)
